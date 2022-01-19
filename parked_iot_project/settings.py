9d0327876212a7ea85f84dd2d1d68b7b4b1dfb1d
L4Limbo <anestis2799@gmail.com>
2022-01-19 15:40:59 +0200

    PG DB url

--- parked_iot_project/settings.py
+++ parked_iot_project/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 from pathlib import Path
 
@@ -84,8 +89,8 @@ WSGI_APPLICATION = 'parked_iot_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': env('DATABASE_URL'),
     }
 }
 
