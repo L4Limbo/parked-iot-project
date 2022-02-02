f1ce13c9ef39ed725124778145a990a8ea1a43e1
Manolis Tzagakis <sxoinatzag@hotmail.gr>
2022-02-03 00:05:32 +0200

    --amend

--- apps/spots/views.py
+++ apps/spots/views.py
@@ -75,7 +75,7 @@ def saveseat(request):
         try:
             sp = Parkingspot.objects.get(spotid=spot_id)
             #print(sp.spotid)
-            if(sp.status == 'free' or sp.status == 'unknown' and distance(gps_lat, sp.spotlat, gps_long, sp.spotlong)<0.01):
+            if((sp.status == 'free' or sp.status == 'unknown') and distance(gps_lat, sp.spotlat, gps_long, sp.spotlong)<0.01):
                 sp.status = 'occupied'
                 sp.save()
                 responseData = {
